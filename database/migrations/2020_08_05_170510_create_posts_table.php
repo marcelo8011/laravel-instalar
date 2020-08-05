0270a7a4e1ed27395c7622899780f76c57b075f7
Marcelo Vásconez <bmvasconez@gmail.com>
2020-08-05 15:40:24 -0700

    h1 -migration posts

--- database/migrations/2020_08_05_170510_create_posts_table.php
+++ database/migrations/2020_08_05_170510_create_posts_table.php
@@ -19,7 +19,7 @@ class CreatePostsTable extends Migration
             $table->text('contenido');
             $table->unsignedBigInteger('user_id');
             $table->timestamps();
-             $table->foreign('user_id')->references('id')->on('users');
+            
         });
     }
 
